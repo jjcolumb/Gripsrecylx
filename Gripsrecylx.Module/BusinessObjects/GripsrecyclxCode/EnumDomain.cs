d46b7b237746d10972e5496d34b6662600295c11
Jose Javier Columbie <josejaviercolumbie@yahoo.com>
2023-08-08 06:39:38 -0700

    Add project files.

---
+++ Gripsrecylx.Module/BusinessObjects/GripsrecyclxCode/EnumDomain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace Gripsrecylx.Module.BusinessObjects.Company1
+{
+
+    public partial class EnumDomain
+    {
+        public EnumDomain(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
